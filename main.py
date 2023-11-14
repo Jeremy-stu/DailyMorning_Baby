0b83dd444d82beed6106ea7edd8db51f5502c950
Jeremy-stu <76563274+Jeremy-stu@users.noreply.github.com>
2023-11-14 09:25:00 +0800

    更新 main.py
    测试3

--- main.py
+++ main.py
@@ -115,7 +115,7 @@ def get_ciba():
     if len(note_en) > 1:
         #note_en2 = note_en[40:]
         #note_en = note_en[:40]
-        note_en = "hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh"
+        note_en = "hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh"
         note_en2 = "完"
     else:
         note_en2 = ""
